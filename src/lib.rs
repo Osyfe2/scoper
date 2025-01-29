11e1c3904292da096c4238213fa7680ed3578040
Osyfe2 <54218896+Osyfe2@users.noreply.github.com>
2025-01-29 11:51:11 +0100

    Macro move

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,25 @@
 #![warn(clippy::all, clippy::perf, clippy::pedantic)]
 
 use std::{num::NonZero, thread::ThreadId};
+
 use eventtypes::EventType;
 use slots::SlotIndex;
 
-mod json;
-mod slots;
-mod recordscope;
 mod eventtypes;
+mod json;
+mod macro_rules;
+mod recordscope;
+mod slots;
 
+pub use global_scope::{record_custom_instant, record_custom_value};
 pub use recordscope::RecordScope;
+
+pub mod macros
+{
+    pub use scoper_attr::record;
+
+    pub use crate::{record_instant, record_scope, record_value};
+}
 
 pub(crate) use std::time::Instant as TimePoint;
 
@@ -21,25 +31,12 @@ struct Key(*const TraceInfo<'static>);
 
 impl Key
 {
-    fn from_static(data: &'static TraceInfo) -> Self { 
-        Self(std::ptr::from_ref::<TraceInfo>(data)) }
+    fn from_static(data: &'static TraceInfo) -> Self { Self(std::ptr::from_ref::<TraceInfo>(data)) }
 
     fn read(self) -> &'static TraceInfo<'static> { unsafe { &*self.0 } }
 }
 
 unsafe impl Send for Key {}
-
-pub mod macros
-{
-    pub use scoper_attr::record;
-
-    pub use crate::record_scope;
-    pub use crate::record_value;
-    pub use crate::record_instant;
-}
-
-pub use global_scope::counter_event;
-pub use global_scope::instant_event;
 
 pub struct Scope
 {
@@ -50,20 +47,19 @@ pub struct Scope
 impl Scope
 {
     #[must_use]
-    pub fn start(data: &'static TraceInfo) -> Option<Self>
+    pub fn start(data: &'static TraceInfo) -> Self
     {
         let level = global_scope::open_scope();
-        Some(Self {
+        Self {
             key: Key::from_static(data),
             level,
-        })
+        }
     }
 }
 
 impl Drop for Scope
 {
-    fn drop(&mut self) { 
-        global_scope::close_scope(self); }
+    fn drop(&mut self) { global_scope::close_scope(self); }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Ord, Eq)]
@@ -79,7 +75,8 @@ impl InstantScopeSize
     #[must_use]
     pub fn code(self) -> char
     {
-        match self {
+        match self
+        {
             InstantScopeSize::Thread => 't',
             InstantScopeSize::Process => 'p',
             InstantScopeSize::Global => 'g',
@@ -87,7 +84,8 @@ impl InstantScopeSize
     }
 }
 
-union TraceAddition {
+union TraceAddition
+{
     end: TimePoint,
     int_float: (i64, f64),
     scope_size: InstantScopeSize,
@@ -95,9 +93,9 @@ union TraceAddition {
 
 struct Trace<const TYPE: EventType>
 {
-    thread_id: ThreadId, //All trace types 
-    start: TimePoint, //All trace types 
-    key: Key, //Key to static info
+    thread_id: ThreadId,     //All trace types
+    start: TimePoint,        //All trace types
+    key: Key,                //Key to static info
     addition: TraceAddition, //non static data
 }
 
@@ -110,29 +108,33 @@ pub struct TraceInfo<'a>
     pub args: &'a str,
 }
 
-
 type Pid = &'static str;
 type Tid = NonZero<u64>;
 
 #[derive(Debug)]
 pub enum MetaTrace
 {
-    ProcessName(Pid, String),        //__metadata M
+    ProcessName(Pid, String), //__metadata M
     //ProcessSortIndex(Pid, usize),        //__metadata M todo!
     //ProcessLabels(Pid, String),        //__metadata M todo!
-    ThreadName(Pid, Tid, String),    //__metadata M
-    //ThreadSortIndex(Pid, Tid, usize),        //__metadata M todo!
-    //ProcessUptimeSeconds(Pid, u128), //__metadata M Not in the doc
-    //ActiveProcesses(Vec<Pid>, u128), //__metadata I s:g Not in the doc
+    ThreadName(Pid, Tid, String), /* M */
+                                  /*ThreadSortIndex(Pid, Tid, usize),        //__metadata M todo!
+                                   *ProcessUptimeSeconds(Pid, u128), //__metadata M Not in the doc
+                                   *ActiveProcesses(Vec<Pid>, u128), //__metadata I s:g Not in the doc */
 }
 
 mod global_scope
 {
     use std::{
-        cell::RefCell, sync::{LazyLock, Mutex, MutexGuard}, thread::{current, ThreadId}
+        cell::RefCell,
+        sync::{LazyLock, Mutex, MutexGuard},
+        thread::{ThreadId, current},
     };
 
-    use super::{EventType, Key, TimePoint, InstantScopeSize, Scope, Trace, TraceInfo, TraceAddition, slots::{Slots, SlotIndex}};
+    use super::{
+        EventType, InstantScopeSize, Key, Scope, TimePoint, Trace, TraceAddition, TraceInfo,
+        slots::{SlotIndex, Slots},
+    };
 
     type OpenScopes = Slots<TimePoint>;
     type Traces<const TYPE: EventType> = Vec<Trace<TYPE>>;
@@ -141,33 +143,23 @@ mod global_scope
         static THREAD_ID: ThreadId = current().id();
         static OPEN_SCOPES: RefCell<OpenScopes> = RefCell::default();
     }
-    static SCOPES: LazyLock<Mutex<Traces<{EventType::Scope}>>> = LazyLock::new(const { || Mutex::new(Traces::new()) });
-    static COUNTERS: LazyLock<Mutex<Traces<{EventType::Counter}>>> = LazyLock::new(const { || Mutex::new(Traces::new()) });
-    static INSTANCES: LazyLock<Mutex<Traces<{EventType::Instant}>>> = LazyLock::new(const { || Mutex::new(Traces::new()) });
+    static SCOPES: LazyLock<Mutex<Traces<{ EventType::Scope }>>> = LazyLock::new(const { || Mutex::new(Traces::new()) });
+    static COUNTERS: LazyLock<Mutex<Traces<{ EventType::Counter }>>> = LazyLock::new(const { || Mutex::new(Traces::new()) });
+    static INSTANCES: LazyLock<Mutex<Traces<{ EventType::Instant }>>> = LazyLock::new(const { || Mutex::new(Traces::new()) });
 
-    fn access_scopes<'a>() -> MutexGuard<'a, Traces<{EventType::Scope}>> { SCOPES.lock().expect("Could not get access") }
-    fn access_counters<'a>() -> MutexGuard<'a, Traces<{EventType::Counter}>> { COUNTERS.lock().expect("Could not get access") }
-    fn access_instances<'a>() -> MutexGuard<'a, Traces<{EventType::Instant}>> { INSTANCES.lock().expect("Could not get access") }
+    fn access_scopes<'a>() -> MutexGuard<'a, Traces<{ EventType::Scope }>> { SCOPES.lock().expect("Could not get access") }
+    fn access_counters<'a>() -> MutexGuard<'a, Traces<{ EventType::Counter }>> { COUNTERS.lock().expect("Could not get access") }
+    fn access_instances<'a>() -> MutexGuard<'a, Traces<{ EventType::Instant }>> { INSTANCES.lock().expect("Could not get access") }
 
-    pub(super) fn flush_traces() -> Traces<{EventType::Scope}> { std::mem::take(&mut access_scopes()) }
-    pub(super) fn flush_counters() -> Traces< {EventType::Counter}> { std::mem::take(&mut access_counters()) }
-    pub(super) fn flush_instances() -> Traces< {EventType::Instant}> { std::mem::take(&mut access_instances()) }
+    pub(super) fn flush_traces() -> Traces<{ EventType::Scope }> { std::mem::take(&mut access_scopes()) }
+    pub(super) fn flush_counters() -> Traces<{ EventType::Counter }> { std::mem::take(&mut access_counters()) }
+    pub(super) fn flush_instances() -> Traces<{ EventType::Instant }> { std::mem::take(&mut access_instances()) }
 
-    pub(super) fn open_scope() -> SlotIndex
-    {
-        OPEN_SCOPES.with_borrow_mut(|slots| {
-            slots.push(TimePoint::now())
-        })
-    }
+    pub(super) fn open_scope() -> SlotIndex { OPEN_SCOPES.with_borrow_mut(|slots| slots.push(TimePoint::now())) }
 
     fn pop_scope_opening_time(level: SlotIndex) -> TimePoint
     {
-        OPEN_SCOPES.with_borrow_mut(
-            |slots|
-            {
-                slots.take(level)
-            }
-        )
+        OPEN_SCOPES.with_borrow_mut(|slots| slots.take(level))
         /*
         if let Some(mut x) = OPEN_SCOPES.with_borrow_mut(Vec::pop)
         {
@@ -190,7 +182,7 @@ mod global_scope
         */
     }
 
-    impl <const TYPE: EventType> Trace<TYPE>
+    impl<const TYPE: EventType> Trace<TYPE>
     {
         fn build(info: &'static TraceInfo, addition: TraceAddition) -> Self
         {
@@ -203,7 +195,7 @@ mod global_scope
         }
     }
 
-    pub(super) fn close_scope(Scope{key, level}: &Scope)
+    pub(super) fn close_scope(Scope { key, level }: &Scope)
     {
         let start = pop_scope_opening_time(unsafe { level.duplicate() });
         let thread_id = THREAD_ID.with(Clone::clone);
@@ -218,78 +210,15 @@ mod global_scope
         access_scopes().push(trace);
     }
 
-    pub fn counter_event(info: &'static TraceInfo, int_float: (i64, f64))
+    pub fn record_custom_value(info: &'static TraceInfo, int_float: (i64, f64))
     {
-        let trace = Trace::build(info, TraceAddition{int_float});
+        let trace = Trace::build(info, TraceAddition { int_float });
         access_counters().push(trace);
     }
 
-    pub fn instant_event(info: &'static TraceInfo, scope_size: InstantScopeSize)
+    pub fn record_custom_instant(info: &'static TraceInfo, scope_size: InstantScopeSize)
     {
-        access_instances().push(Trace::build(info, TraceAddition{scope_size}));
-    }
-}
-
-pub mod macro_rules
-{
-    pub use const_format::str_replace;
-
-    #[macro_export]
-    macro_rules! record_scope {
-        ($header: expr, $name: expr) => {
-            static TRACE_SCOPE_INFO: $crate::TraceInfo = $crate::TraceInfo {
-                name: $name,
-                category: $crate::macro_rules::str_replace!(::std::module_path!(), "::", ","),
-                header: $header,
-                args: "",
-            };
-            #[allow(unused)]
-            let _profiling_scope = $crate::Scope::start(&TRACE_SCOPE_INFO);
-        };
-        ($name: expr) => {
-            record_scope!("", $name)
-        };
-    }
-
-    #[macro_export]
-    macro_rules! record_value {
-        ($header: expr, $name: expr, $int: expr, $float: expr) => {
-            {
-                static TRACE_COUNTER_INFO: $crate::TraceInfo = $crate::TraceInfo {
-                    name: $name,
-                    category: $crate::macro_rules::str_replace!(::std::module_path!(), "::", ","),
-                    header: $header,
-                    args: "",
-                };
-                $crate::counter_event(&TRACE_COUNTER_INFO, ($int, $float));
-            }
-        };
-        /*
-        ($name: expr) => {
-            record_value!("", $name)
-        };
-        */
-    }
-
-    #[macro_export]
-    macro_rules! record_instant {
-        ($header: expr, $name: expr, $scope_size: expr) => {
-            {
-                static TRACE_INSTANT_INFO: $crate::TraceInfo = $crate::TraceInfo {
-                    name: $name,
-                    category: $crate::macro_rules::str_replace!(::std::module_path!(), "::", ","),
-                    header: $header,
-                    args: "",
-                };
-                $crate::instant_event(&TRACE_INSTANT_INFO, $scope_size);
-            }
-        };
-        ($name: expr, $scope_size: expr) => {
-            record_instant!("", $name, $scope_size);
-        };
-        ($name: expr) => {
-            record_instant!($name, $crate::InstantScopeSize::Process);
-        };
+        access_instances().push(Trace::build(info, TraceAddition { scope_size }));
     }
 }
 
@@ -298,7 +227,7 @@ mod test
 {
     use std::{path::Path, thread::sleep, time::Duration};
 
-    use crate::{global_scope::counter_event, macros::*, RecordScope};
+    use crate::{RecordScope, global_scope::record_custom_value, macros::*};
     fn wait_30_ms()
     {
         use crate::{Scope, TraceInfo};
@@ -312,7 +241,7 @@ mod test
 
         let _profiling_scope = Scope::start(&SCOPE_INFO);
         let value = 0.8;
-        counter_event(&SCOPE_INFO, (0, value));
+        record_custom_value(&SCOPE_INFO, (0, value));
         sleep(Duration::from_millis(30));
     }
 
@@ -387,6 +316,8 @@ mod test
                 }
             });
         });
-        record.add_meta_data("test".to_string(), &String::from("SomeExtraInfoHere")).ok();
+        record
+            .add_meta_data("test".to_string(), &String::from("SomeExtraInfoHere"))
+            .ok();
     }
 }
---
+++ src/macro_rules.rs
@@ -0,0 +1,60 @@
+
+pub use const_format::str_replace;
+
+#[macro_export]
+macro_rules! record_scope {
+    ($header: expr, $name: expr) => {
+        static TRACE_SCOPE_INFO: $crate::TraceInfo = $crate::TraceInfo {
+            name: $name,
+            category: $crate::macro_rules::str_replace!(::std::module_path!(), "::", ","),
+            header: $header,
+            args: "",
+        };
+        #[allow(unused)]
+        let _profiling_scope = $crate::Scope::start(&TRACE_SCOPE_INFO);
+    };
+    ($name: expr) => {
+        record_scope!("", $name)
+    };
+}
+
+#[macro_export]
+macro_rules! record_value {
+    ($header: expr, $name: expr, $int: expr, $float: expr) => {
+        {
+            static TRACE_COUNTER_INFO: $crate::TraceInfo = $crate::TraceInfo {
+                name: $name,
+                category: $crate::macro_rules::str_replace!(::std::module_path!(), "::", ","),
+                header: $header,
+                args: "",
+            };
+            $crate::record_custom_value(&TRACE_COUNTER_INFO, ($int, $float));
+        }
+    };
+    /*
+    ($name: expr) => {
+        record_value!("", $name)
+    };
+    */
+}
+
+#[macro_export]
+macro_rules! record_instant {
+    ($header: expr, $name: expr, $scope_size: expr) => {
+        {
+            static TRACE_INSTANT_INFO: $crate::TraceInfo = $crate::TraceInfo {
+                name: $name,
+                category: $crate::macro_rules::str_replace!(::std::module_path!(), "::", ","),
+                header: $header,
+                args: "",
+            };
+            $crate::record_custom_instant(&TRACE_INSTANT_INFO, $scope_size);
+        }
+    };
+    ($name: expr, $scope_size: expr) => {
+        record_instant!("", $name, $scope_size);
+    };
+    ($name: expr) => {
+        record_instant!($name, $crate::InstantScopeSize::Process);
+    };
+}
