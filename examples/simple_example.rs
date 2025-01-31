72876515860b28a0ef6b0e37079e96444d7d4b88
Osyfe2 <54218896+Osyfe2@users.noreply.github.com>
2025-01-31 16:15:53 +0100

    simple example

--- examples/simple_example.rs
+++ examples/simple_example.rs
@@ -1,6 +1,19 @@
+use std::{path::Path, thread::sleep, time::Duration};
 
+use scoper::{RecordScope, macros::*};
+
+#[record]
+fn do_some_work() { sleep(Duration::from_millis(30)); }
+
+#[record]
+fn other_work() { sleep(Duration::from_millis(5)); }
 
 fn main()
 {
-    
+    let _record = RecordScope::start(Path::new("results/simple_example.json"));
+    for _ in 0..6
+    {
+        do_some_work();
+        other_work();
+    }
 }
